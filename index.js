3b28b7f8f19ec658869eb6d5ac8b02dfadf66638
Abdullah-563014 <md.abdullah563014@gmail.com>
2022-07-02 18:06:15 +0600

    random

--- index.js
+++ index.js
@@ -30,9 +30,6 @@ async function startScrapping() {
     await page.setCookie(...cookies);
 
 
-
-
-
     let pageInfo = await loadProductListingPageAndGetInfo(wordpressThemePage);
 
     console.log("==================================================");
@@ -42,30 +39,16 @@ async function startScrapping() {
     allData.pop();
 
 
-    // const sleep = ms => {
-    //   return new Promise(resolve => setTimeout(resolve, ms))
-    // }
-
-    // const getNumFruit = index => {
-    //   return sleep(100).then(v => pageInfo[index])
-    // }
-
-    // if(pageInfo.length) {
-    //   for(let i =0; i <pageInfo.length-1; i++) {
-    //     let info = await getNumFruit(i)
-    //     console.log(info);
-    //     console.log(i+" :=====================================================");
-    //   }
-    // }
-
     if(pageInfo.length) {
       for(let i =0; i <pageInfo.length-1; i++) {
-        let info = await getNumFruit(i)
-        console.log(info);
-        console.log(i+" :=====================================================");
+        let info = await loadProductDetailsPageAndGetInfo(pageInfo[i].productDetailsUrl);
+        pageInfo[i].shortTitle = info.shortTitle;
+        pageInfo[i].downloadLink = info.downloadLink;
+        pageInfo[i].shortDescription = info.shortDescription;
+        pageInfo[i].lognDescription = info.lognDescription;
       }
     }
-    // console.log(pageInfo);
+    console.log(pageInfo);
 
     // writeDataToCsv();
     
@@ -119,7 +102,6 @@ async function loadProductListingPageAndGetInfo(pageUrl) {
     }
 
     
-
     let isAvailableNextPage = document.querySelector("a.next.page-number");
     if(isAvailableNextPage) {
       allInfo.push({
@@ -132,11 +114,8 @@ async function loadProductListingPageAndGetInfo(pageUrl) {
         link: ""
       });
     }
+
     return allInfo;
-
-
-      
-
   });
 
   return pageInfo;
@@ -147,60 +126,33 @@ async function loadProductDetailsPageAndGetInfo(pageUrl) {
   
   await page.goto(pageUrl, {
       timeout: 900000,
-      waitUntil: 'networkidle0',
+      // waitUntil: 'networkidle0',
   });
 
   const pageInfo = await page.evaluate(async () => {
-    let allInfo = [];
 
-    const products = document.querySelectorAll(".products .product-small+.product");
-    for (let i = 0; i< products.length; i++) {
-      const singleProduct = products[i];
-      const category = singleProduct.querySelector(".category").innerText;
-      const imageUrl = singleProduct.querySelector(".box-image img.attachment-woocommerce_thumbnail").src;
-      const title = singleProduct.querySelector("p.product-title a").innerText;
-      let rating;
-      let originalPrice;
-      let discountedPrice;
-      try {
-        rating = singleProduct.querySelector(".price-wrapper .rating").innerText;
-        const priceRoot = singleProduct.querySelectorAll(".price-wrapper .price .woocommerce-Price-amount");
-        originalPrice = priceRoot[0].innerText;
-        discountedPrice = priceRoot[1].innerText;
-      } catch(e) {
-        console.log(e.message);
+    let shortTitle = "";
+    let shortDescription = "";
+    let downloadLink = "";
+    let lognDescription = "";
+    try {
+      shortTitle = document.querySelector(".product-title").innerText;
+      shortDescription = document.querySelector(".product-short-description ul").outerHTML;
+      downloadLink = document.querySelector(".product-short-description a.red-link").href;
+      if(document.getElementById("tab-description")) {
+        lognDescription = document.getElementById("tab-description").innerHTML;
       }
-      const singleInfo = {
-        category: category,
-        imageUrl: imageUrl,
-        productDetailsUrl: productDetailsUrl ? productDetailsUrl : "",
-        title: title,
-        rating: rating,
-        originalPrice: originalPrice ? originalPrice : "0",
-        discountedPrice: discountedPrice ? discountedPrice : "0"
-      };
-      allInfo.push(singleInfo);
+    } catch(e) {
+      console.log(e.message);
     }
+    const details = {
+      shortTitle: shortTitle ? shortTitle : "",
+      shortDescription: shortDescription ? shortDescription : "",
+      downloadLink: downloadLink ? downloadLink : "",
+      lognDescription: lognDescription ? lognDescription : "",
+    };
 
-    
-
-    let isAvailableNextPage = document.querySelector("a.next.page-number");
-    if(isAvailableNextPage) {
-      allInfo.push({
-        status: true,
-        link: isAvailableNextPage.href
-      });
-    } else {
-      allInfo.push({
-        status: false,
-        link: ""
-      });
-    }
-    return allInfo;
-
-
-      
-
+    return details;
   });
 
   return pageInfo;
